3047d7c3a7991e7264b588898928cdf94490f5c6
Mascedo <americomascedo@gmail.com>
2026-02-02 21:14:23 -0300

    esqueci

--- src/models/resumoModel.js
+++ src/models/resumoModel.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const Resumo = mongoose.model('Resumo', {
     data: String,
     cidade: String,
+    cidadeUrlSafe: String,
     resumo: String,
     apiCall: Object
 })
