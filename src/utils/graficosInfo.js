468f53f24a21e6704dd2b4dc7da41b6ca609078a
Mascedo <americomascedo@gmail.com>
2026-02-15 12:49:53 -0300

    horarios agora é array de number

--- src/utils/graficosInfo.js
+++ src/utils/graficosInfo.js
@@ -20,7 +20,7 @@ export async function chamarOpenMeteograficoInfo(latitude, longitude, data){
 
         let maxTemp = -100
 
-        let horarios = ["00h","01h","02h","03h","04h","05h","06h","07h","08h","09h","10h","11h","12h","13h","14h","15h","16h","17h","18h","19h","20h","21h","22h","23h"]
+        let horarios = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 
 
         temperaturas.forEach(temperatura => {
