451047d12e09d50f90e6c2bb73fce1c13d5c2d8c
Mascedo <americomascedo@gmail.com>
2026-02-15 11:26:01 -0300

    horarios no graficoInfo

--- src/utils/graficosInfo.js
+++ src/utils/graficosInfo.js
@@ -20,6 +20,9 @@ export async function chamarOpenMeteograficoInfo(latitude, longitude, data){
 
         let maxTemp = -100
 
+        let horarios = ["00h","01h","02h","03h","04h","05h","06h","07h","08h","09h","10h","11h","12h","13h","14h","15h","16h","17h","18h","19h","20h","21h","22h","23h"]
+
+
         temperaturas.forEach(temperatura => {
             if(temperatura < minTemp){
                 minTemp = temperatura
@@ -34,7 +37,7 @@ export async function chamarOpenMeteograficoInfo(latitude, longitude, data){
 
         const minMaxTemp = minTemp + "°C / " + maxTemp + "°C"
 
-        return [minMaxTemp, temperaturas, temperaturasAparentes, precipitacoes]
+        return [minMaxTemp, temperaturas, temperaturasAparentes, precipitacoes, horarios]
 
     } catch (error) {
         console.error("Erro ao chamar a API:", error.message);
